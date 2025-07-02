d9af32146d0f67db0b2ab9aa1b6f052ec325d7cb
Andrea Franceschini <andrea.franceschini@gmail.com>
2025-07-02 13:03:31 +0200

    :sparkles: We can now load and play and stop individual test files. Loads to do more but this is good.

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -4,33 +4,30 @@ import Listener from "@/app/classes/Listener";
 import { isDefined } from "@/app/miscTools";
 import { ClientOnly } from "@bkwld/next-client-only";
 import { useEffect, useState } from "react";
+import test_files from "./test_files.json";
+import { Nullable } from "../soundrise-types";
 
 export default function Tests() {
+
+    type TestItem = {
+        filename: string,
+        vowel: string,
+        isPlaying?: Nullable<boolean>,
+        audioBufferSourceNode?: Nullable<AudioBufferSourceNode>,
+    }
+
     let [audioContext, setAudioContext] = useState<AudioContext>()
     const [listener, setListener] = useState<Listener>()
     const [audioFiles, setAudioFiles] = useState<AudioBuffer[]>([])
+    const [testItems, setTestItems] = useState<TestItem[]>()
     
     useEffect(() => {
         setAudioContext(new AudioContext())
         setListener(new Listener())
+
+        const ti = test_files.map(f => f as TestItem)
+        setTestItems(ti)
     }, [])
-
-    useEffect(() => {
-        if (!isDefined(listener) || !isDefined<AudioContext>(audioContext)) return
-
-        listener.audioContext = audioContext
-        fetch('/test_assets/87778__marcgascon7__vocals.wav')
-        .then(response => {
-            return response.arrayBuffer()
-        })
-        .then(buffer => {
-            audioContext?.decodeAudioData(buffer, decoded => {
-                audioFiles.push(decoded)
-            })
-            doTheThing()
-        })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [listener, audioContext])
 
     function doTheThing() {
         if (!isDefined(listener) || !isDefined(audioContext)) return
@@ -40,15 +37,82 @@ export default function Tests() {
         audioBufferSourceNode.loop = false
         audioBufferSourceNode.connect(audioContext.destination)
         listener.mediaStreamSource = audioBufferSourceNode
-        console.log(listener)
         listener.startListening()
         audioBufferSourceNode.start()
+    }
+
+    function playTestItem(i: number) {
+        if (!isDefined(listener) || !isDefined(audioContext) || !isDefined(testItems)) return
+
+        const ti = [...testItems]
+
+        listener.audioContext = audioContext
+        fetch('/test_assets/' + ti[i].filename)
+        .then(response => {
+            return response.arrayBuffer()
+        })
+        .then(buffer => {
+            audioContext?.decodeAudioData(buffer, decoded => {
+                ti[i].audioBufferSourceNode = audioContext.createBufferSource()
+                ti[i].audioBufferSourceNode.buffer = decoded
+                ti[i].audioBufferSourceNode.loop = false
+                ti[i].audioBufferSourceNode.connect(audioContext.destination)
+                listener.mediaStreamSource = ti[i].audioBufferSourceNode
+                listener.startListening()
+                ti[i].audioBufferSourceNode.onended = () => stopTestItem(i)
+                ti[i].audioBufferSourceNode.start()
+
+                ti[i].isPlaying = true
+                setTestItems(ti)
+
+            })
+        })
+
+    }
+
+    function stopTestItem(i: number) {
+        if (!isDefined(testItems) ) return
+        const ti = [...testItems]
+        if (!isDefined(ti[i].audioBufferSourceNode)) return
+        ti[i].audioBufferSourceNode.stop()
+        ti[i].audioBufferSourceNode.disconnect()
+        ti[i].audioBufferSourceNode.buffer = null
+        ti[i].audioBufferSourceNode = null
+        ti[i].isPlaying = false
+        setTestItems(ti)
     }
 
     return (
         <ClientOnly>
             <div>
-                <h1>Tests</h1>
+                <h1 className="text-white text-2xl text-center p-6">audio tests</h1>
+
+                <div className="header">
+                    <div className="row p-2 bg-white grid grid-cols-12">
+                        <div></div>
+                        <div className="text-center">vowel</div>
+                        <div className="col-span-2">test result</div>
+                        <div className="col-span-8">filename</div>
+                    </div>
+                </div>
+                <div className="body pb-2 bg-blue-100">
+                    {testItems && testItems.map((item, i) =>
+                    <div className="row p-2 pb-0 grid grid-cols-12" key={i}>
+                        <div className="text-center">
+                            <button className="btn" onClick={() => testItems[i].isPlaying ? stopTestItem(i) : playTestItem(i)}>
+                                {item.isPlaying ?
+                                <span className="square-icon text-lg">Stop</span>
+                                :
+                                <span className="triangle-icon text-lg">Play</span>
+                                }
+                            </button>
+                        </div>
+                        <div className="text-center">{item.vowel}</div>
+                        <div className="col-span-2"></div>
+                        <div className="col-span-8">{item.filename}</div>
+                    </div>)
+                    }
+                </div>
             </div>
         </ClientOnly>
     )
